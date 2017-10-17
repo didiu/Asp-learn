d560834b86214215195e8913e0badcb4b3f1e32d
v-guax <v-guax@MSSTORE-GUANGTU>
2017-10-17 14:48:11 +0800

    自定义异常类

---
+++ 自定义异常处理/自定义异常处理/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 自定义异常处理
+{
+
+    #region 自定义的异常类
+    public class IsZeroException :ApplicationException
+    {
+        public IsZeroException(string message):base(message)
+        {
+
+        }
+    }
+    #endregion
+
+    #region 实现类
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw (new IsZeroException("Zero Temperature found"));
+            }
+            catch (IsZeroException e)
+            {
+                Console.WriteLine("IsZeroException:{0}", e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+    #endregion
+}
